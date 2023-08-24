48c72db587cc50d1c2415cfdbbd0040ea7b9d6c7
Elvis Estevez <124850509+ElvisAbner@users.noreply.github.com>
2023-08-24 20:58:45 +0000

    update server.js

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 app.use(express.static(__dirname + '/src/public'));
 
-app.use(bodyParser.urlencoded({
+app.use(express.urlencoded({
   extended: true
 }));
 
